afcbe5f82a35c23781e2b6f3155b21a730df2015
Ali Helmi <ahelmi365@gmail.com>
2022-07-09 17:33:45 +0200

    Angualr Lesson 3

---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from '../models/Post';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent implements OnInit {
+  @Input() post:Post;
+  @Output() hidePost:EventEmitter<Post> = new EventEmitter;
+
+  constructor() {
+    this.post={
+      id:0,
+      title:'',
+      body:'',
+      votes:1
+    }
+   }
+
+  ngOnInit(): void {
+  }
+
+  upVote(post:Post):void{
+    post.votes+=1;
+  }
+
+  downVote(post:Post):void{
+    if(post.votes>0){
+    post.votes-=1;
+    }
+  }
+
+  hide(post:Post):void{
+    this.hidePost.emit(post);
+  }
+
+}
