31b34a80023cc8080e7ab6d41bdd76d60b7195a0
Ali Helmi <ahelmi365@gmail.com>
2022-07-09 19:37:22 +0200

    update post-item comp

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,56 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { filter } from 'rxjs';
+// import { filter } from 'rxjs';
 import { Post } from '../models/Post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent implements OnInit  {
-  title:string="Posts Component";
-  posts:Post[]=[];
-   
+export class PostsComponent implements OnInit {
+  title: string = 'Posts Component';
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.posts=[
+    this.posts = [
       {
-        id:1,
-        title:"My 1st post",
-        body:"My 1st body",
-        votes:1
+        id: 1,
+        title: 'My 1st post',
+        body: 'My 1st body',
+        votes: 1,
       },
       {
-        id:2,
-        title:"My 2nd post",
-        body:"My 2nd body",
-        votes:2
+        id: 2,
+        title: 'My 2nd post',
+        body: 'My 2nd body',
+        votes: 2,
       },
       {
-        id:3,
-        title:"My 3rd post",
-        body:"My 3rd body",
-        votes:3
+        id: 3,
+        title: 'My 3rd post',
+        body: 'My 3rd body',
+        votes: 3,
       },
       {
-        id:4,
-        title:"My 4th post",
-        body:"My 4th body",
-        votes:4
+        id: 4,
+        title: 'My 4th post',
+        body: 'My 4th body',
+        votes: 4,
       },
       {
-        id:5,
-        title:"My 5th post",
-        body:"My 5th body",
-        votes:5
+        id: 5,
+        title: 'My 5th post',
+        body: 'My 5th body',
+        votes: 5,
       },
-    ]
-  }
- 
-  hidePost(post:Post):void{
-    this.posts=this.posts.filter(p=> p.id !== post.id)
+    ];
   }
 
+  hidePost(post: Post): void {
+    this.posts = this.posts.filter((p) => p.id !== post.id);
+  }
 }
